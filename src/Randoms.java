934d7432ff3b84725297814b50af3adffaf3ad7e
Boris Kaloshin <BORIS@MacBook-Pro.local>
2022-08-29 14:38:04 +0300

    added pattern iterator

---
+++ src/Randoms.java
@@ -0,0 +1,30 @@
+import java.util.Iterator;
+import java.util.Random;
+
+public class Randoms implements Iterable<Integer> {
+    private final Random random;
+    private final int min;
+    private final int max;
+
+    public Randoms(int min, int max) {
+        this.min = min;
+        this.max = max;
+        this.random = new Random();
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+
+        return new Iterator<>() {
+            @Override
+            public boolean hasNext() {
+                return true;
+            }
+
+            @Override
+            public Integer next() {
+                return min + random.nextInt(max - min + 1);
+            }
+        };
+    }
+}
